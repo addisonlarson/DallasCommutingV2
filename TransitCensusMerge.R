4b1582f5d9982343ed89c3c12c531de9e08e576b
Addison Larson <39769450+addisonlarson@users.noreply.github.com>
2018-08-22 18:37:30 -0400

    script to merge transit shapefile with Census API extracts

--- TransitCensusMerge.R
+++ TransitCensusMerge.R
@@ -23,7 +23,201 @@ MSAshps <- subset(MSAshps, !(id %in% drops))
 MSAshps <- sub("\\.shp$", "", MSAshps$id) 
 
 for (i in 1:length(MSAshps)) {
-  setwd("D:/AP LARSON/DallasCommutingV2/tractShps") # ? why not working ?
   assign(MSAshps[i], readOGR(".", MSAshps[i]))
 }
-# so BIR_AL has tiny file sizes indicating a bad merge in TransitSpatialTransform
+
+setwd("D:/AP LARSON/DallasCommutingV2/censusData")
+MSAcsvs <- list.files(pattern = "\\.csv$")
+for (i in 1:length(MSAcsvs)){
+  assign(MSAcsvs[i], read.csv(MSAcsvs[i]))
+}
+
+setwd("D:/AP LARSON/DallasCommutingV2/tractShpsCensus")
+# 1. ATLANTA, GA
+currentShp <- merge(res_ATL_GA, `dATLANTA, GA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_ATL_GA", driver = "ESRI Shapefile")
+
+# 2. BALTIMORE, MD
+currentShp <- merge(res_BAL_MD, `dBALTIMORE, MD.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_BAL_MD", driver = "ESRI Shapefile")
+
+# 3. BIRMINGHAM, AL
+currentShp <- merge(res_BIR_AL, `dBIRMINGHAM, AL.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_BIR_AL", driver = "ESRI Shapefile")
+
+# 4. BOSTON, MA
+currentShp <- merge(res_BOS_MA, `dBOSTON-WORCESTER-LAWRENCE-LOWELL-BROCKTON, MA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_BOS_MA", driver = "ESRI Shapefile")
+
+# 5. BUFFALO, NY
+currentShp <- merge(res_BUF_NY, `dBUFFALO-NIAGARA FALLS, NY.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_BUF_NY", driver = "ESRI Shapefile")
+
+# 6. CHARLOTTE, NC
+currentShp <- merge(res_CHA_NC, `dCHARLOTTE-GASTONIA-ROCK HILL, NC-SC.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_CHA_NC", driver = "ESRI Shapefile")
+
+# 7. CINCINNATI, OH
+currentShp <- merge(res_CIN_OH, `dCINCINNATI, OH-KY-IN.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_CIN_OH", driver = "ESRI Shapefile")
+
+# 8. CLEVELAND, OH
+currentShp <- merge(res_CLE_OH, `dCLEVELAND-LORAIN-ELYRIA, OH.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_CLE_OH", driver = "ESRI Shapefile")
+
+# 9. COLUMBUS, OH
+currentShp <- merge(res_COL_OH, `dCOLUMBUS, OH.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_COL_OH", driver = "ESRI Shapefile")
+
+# 10. DENVER, CO
+currentShp <- merge(res_DEN_CO, `dDENVER, CO.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_DEN_CO", driver = "ESRI Shapefile")
+
+# 11. HAMILTON, OH
+currentShp <- merge(res_HAM_OH, `dHAMILTON-MIDDLETOWN, OH.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_HAM_OH", driver = "ESRI Shapefile")
+
+# 12. INDIANAPOLIS, IN
+currentShp <- merge(res_IND_IN, `dINDIANAPOLIS, IN.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_IND_IN", driver = "ESRI Shapefile")
+
+# 13. KANSAS CITY, MO
+currentShp <- merge(res_KAN_MO, `dKANSAS CITY, MO-KS.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_KAN_MO", driver = "ESRI Shapefile")
+
+# 14. LOUISVILLE, KY
+currentShp <- merge(res_LOU_KY, `dLOUISVILLE, KY-IN.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_LOU_KY", driver = "ESRI Shapefile")
+
+# 15. MINNEAPOLIS, MN
+currentShp <- merge(res_MIN_MN, `dMINNEAPOLIS-ST. PAUL, MN-WI.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_MIN_MN", driver = "ESRI Shapefile")
+
+# 16. NEW ORLEANS, LA
+currentShp <- merge(res_NEW_LA, `dNEW ORLEANS, LA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_NEW_LA", driver = "ESRI Shapefile")
+
+# 17. NORFOLK, VA
+currentShp <- merge(res_NOR_VA, `dNORFOLK-VIRGINIA BEACH-NEWPORT NEWS, VA-NC.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_NOR_VA", driver = "ESRI Shapefile")
+
+# 18. PITTSBURGH, PA
+currentShp <- merge(res_PIT_PA, `dPITTSBURGH, PA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_PIT_PA", driver = "ESRI Shapefile")
+
+# 19. PORTLAND, OR
+currentShp <- merge(res_POR_OR, `dPORTLAND-VANCOUVER,OR-WA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_POR_OR", driver = "ESRI Shapefile")
+
+# 20. PROVIDENCE, RI
+currentShp <- merge(res_PRO_RI, `dPROVIDENCE-WARWICK-PAWTUCKET, RI.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_PRO_RI", driver = "ESRI Shapefile")
+
+# 21. RALEIGH-DURHAM, NC
+currentShp <- merge(res_RAL_NC, `dRALEIGH-DURHAM-CHAPEL HILL, NC.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_RAL_NC", driver = "ESRI Shapefile")
+
+# 22. SACRAMENTO, CA
+currentShp <- merge(res_SAC_CA, `dSACRAMENTO, CA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_SAC_CA", driver = "ESRI Shapefile")
+
+# 23. SAN DIEGO, CA
+currentShp <- merge(res_SAND_CA, `dSAN DIEGO, CA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_SAND_CA", driver = "ESRI Shapefile")
+
+# 24. SAN JOSE, CA
+currentShp <- merge(res_SANJ_CA, `dSAN JOSE, CA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_SANJ_CA", driver = "ESRI Shapefile")
+
+# 25. ST. LOUIS, MO
+currentShp <- merge(res_STL_MO, `dST. LOUIS, MO-IL.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_STL_MO", driver = "ESRI Shapefile")
+
+# 26. TAMPA, FL
+currentShp <- merge(res_TAM_FL, `dTAMPA-ST. PETERSBURG-CLEARWATER, FL.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_TAM_FL", driver = "ESRI Shapefile")
+
+# 27. YOLO, CA
+currentShp <- merge(res_YOL_CA, `dYOLO, CA.csv`,
+                    by = "GEOID")
+currentShp$ALAND <- as.numeric(as.character(currentShp$ALAND))
+currentShp$popDens <- currentShp$popData / (currentShp$ALAND * 0.00000038610)
+writeOGR(currentShp, ".", "mer_YOL_CA", driver = "ESRI Shapefile")
